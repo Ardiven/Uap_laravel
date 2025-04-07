c1693a9b05b02579024cbed8e8ba7dacbdc65648
Victor Loka <ardiven.lentera@gmail.com>
2025-04-07 22:02:15 +0700

    dev dash

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,13 +7,13 @@
     @vite('resources/css/app.css')
     <script src="//unpkg.com/alpinejs" defer></script>
 </head>
-<body class="bg-gray-100 ">
+<body class="bg-gray-100 min-h-screen ">
     <!-- Container Utama -->
     <div class="container mx-auto p-4">
         @yield('content')
     </div>
     <!-- Footer -->
-    <footer class="bg-gray-200 text-center py-4 mt-6">
+    <footer class="bg-gray-200 text-center py-4 mt-4">
         <p class="text-gray-600">© {{ date('Y') }} MySchool. All rights reserved.</p>
     </footer>
 </body>
